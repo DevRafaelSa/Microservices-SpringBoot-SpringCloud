69c0b30183f981eb87aa28f57d29123cfd292b3a
Rafael Sá <sanunesrafael@gmail.com>
2023-01-02 11:41:18 -0300

    adaptando para usar melhor o load balancer

--- microservices/currency-conversion-service/src/main/java/com/rafael/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ microservices/currency-conversion-service/src/main/java/com/rafael/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//para fazer o balanceio entre multiplas instancias basta colocar o nome da aplicacao(retirando a url)
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
