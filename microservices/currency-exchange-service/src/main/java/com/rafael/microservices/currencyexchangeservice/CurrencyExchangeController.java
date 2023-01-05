26d6a9ef14b243e7877e42b450d142946a92f9b1
Rafael Sá <sanunesrafael@gmail.com>
2023-01-05 01:49:07 -0300

    utilizando zipkin para rastrear servicos

--- microservices/currency-exchange-service/src/main/java/com/rafael/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/rafael/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,9 @@
 package com.rafael.microservices.currencyexchangeservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactoryFriend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +16,19 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchangeRepository repository;
 
+    //add log
+    private final Logger log = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     //classe Environment ajuda a pegar o valor da porta
     @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangevalue(@PathVariable String from, @PathVariable String to) {
+
+        //add log
+        log.info("retrieveExchangevalue called with {} to {}", from, to);
+
         //passo o "endereco" de onde a classe Environment vai pegar a porta correta e depois passo como
         //parametro no metodo setEnvironment da classe CurrencyExchange
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
